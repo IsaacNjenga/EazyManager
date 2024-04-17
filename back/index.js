6116889d03a25edcfde40099e79d500e64adade1
Isaac Njenga <111574568+IsaacNjenga@users.noreply.github.com>
2024-04-17 13:52:25 +0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors( {
     origin:["https://eazy-manager-front.vercel.app"],
-    methods: ["POST", "GET", "PUT"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
   }));
 
