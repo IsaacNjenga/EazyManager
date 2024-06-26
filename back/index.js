4e1497cbced85a5afb1c2d08e404080700e97c8b
Isaac Njenga <ayzzoh20@gmail.com>
2024-06-26 12:38:45 +0300

    fixed profile

--- back/index.js
+++ back/index.js
@@ -472,9 +472,29 @@ app.get("/check-session", (req, res) => {
   }
 });
 
-app.get("/profile", (req, res) => {
-  const { token } = req.cookies;
-  if (token) {
+app.get("/profile", (req, res, next) => {
+  //const { token } = req.cookies;
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+      try {
+        if (err) {
+          return res.status(401).json({ error: "Unauthorised" });
+        }
+        const user = await UserModel.findOne({ _id: payload._id }).select(
+          "-password"
+        );
+        req.user = user;
+        next();
+      } catch (error) {
+        return res.status(500).json({ error: err.message });
+      }
+    });
+  } else {
+    return res.status(403).json({ error: "Forbidden" });
+  }
+  /*if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
         console.error("JWT verification error:", err);
@@ -487,7 +507,7 @@ app.get("/profile", (req, res) => {
     });
   } else {
     res.status(401).json({ error: "No token found" });
-  }
+  }*/
 });
 
 app.get("/logout", async (req, res) => {
